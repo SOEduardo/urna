a8a3dd2904f106cc65d9b79da92031984b149428
SOEduardo <147120381+SOEduardo@users.noreply.github.com>
2023-11-19 23:07:57 -0300

    Add files via upload

---
+++ Urna do JC/FormResultado.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Urna_do_JC
+{
+    partial class FormResultado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormResultado));
+            label1 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            label2 = new Label();
+            lbl_Carlos = new Label();
+            lbl_Dimitri = new Label();
+            lbl_Gabriel = new Label();
+            lbl_Gustavo = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(312, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(230, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Resultado Final:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(631, 331);
+            label5.Name = "label5";
+            label5.Size = new Size(80, 22);
+            label5.TabIndex = 20;
+            label5.Text = "Gustavo";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(139, 329);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 22);
+            label4.TabIndex = 19;
+            label4.Text = "Gabriel";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(598, 356);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(143, 175);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 18;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(102, 356);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(143, 175);
+            pictureBox3.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox3.TabIndex = 17;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(598, 116);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(143, 158);
+            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox2.TabIndex = 16;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(102, 116);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(143, 158);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 15;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(642, 91);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 22);
+            label3.TabIndex = 22;
+            label3.Text = "Dimitri";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(139, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 22);
+            label2.TabIndex = 21;
+            label2.Text = "Carlos";
+            // 
+            // lbl_Carlos
+            // 
+            lbl_Carlos.AutoSize = true;
+            lbl_Carlos.Font = new Font("Arial", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            lbl_Carlos.Location = new Point(137, 277);
+            lbl_Carlos.Name = "lbl_Carlos";
+            lbl_Carlos.Size = new Size(0, 23);
+            lbl_Carlos.TabIndex = 23;
+            // 
+            // lbl_Dimitri
+            // 
+            lbl_Dimitri.AutoSize = true;
+            lbl_Dimitri.Font = new Font("Arial", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            lbl_Dimitri.Location = new Point(638, 277);
+            lbl_Dimitri.Name = "lbl_Dimitri";
+            lbl_Dimitri.Size = new Size(67, 23);
+            lbl_Dimitri.TabIndex = 24;
+            lbl_Dimitri.Text = "label7";
+            // 
+            // lbl_Gabriel
+            // 
+            lbl_Gabriel.AutoSize = true;
+            lbl_Gabriel.Font = new Font("Arial", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            lbl_Gabriel.Location = new Point(139, 534);
+            lbl_Gabriel.Name = "lbl_Gabriel";
+            lbl_Gabriel.Size = new Size(67, 23);
+            lbl_Gabriel.TabIndex = 25;
+            lbl_Gabriel.Text = "label8";
+            // 
+            // lbl_Gustavo
+            // 
+            lbl_Gustavo.AutoSize = true;
+            lbl_Gustavo.Font = new Font("Arial", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            lbl_Gustavo.Location = new Point(638, 534);
+            lbl_Gustavo.Name = "lbl_Gustavo";
+            lbl_Gustavo.Size = new Size(67, 23);
+            lbl_Gustavo.TabIndex = 26;
+            lbl_Gustavo.Text = "label9";
+            // 
+            // FormResultado
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(843, 647);
+            Controls.Add(lbl_Gustavo);
+            Controls.Add(lbl_Gabriel);
+            Controls.Add(lbl_Dimitri);
+            Controls.Add(lbl_Carlos);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "FormResultado";
+            Text = "FormResultado";
+            Load += FormResultado_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label5;
+        private Label label4;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Label label2;
+        private Label lbl_Carlos;
+        private Label lbl_Dimitri;
+        private Label lbl_Gabriel;
+        private Label lbl_Gustavo;
+    }
+}
---
+++ Urna do JC/FormVotador.cs
@@ -0,0 +1,10 @@
+namespace Urna_do_JC
+{
+    public partial class FormVotador : Form
+    {
+        public FormVotador()
+        {
+            InitializeComponent();
+        }
+    }
+}
